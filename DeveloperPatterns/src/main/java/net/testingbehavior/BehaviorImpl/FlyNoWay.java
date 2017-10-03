068d8fe220385a4941de6ded85e9eab96ec4410c
MaximBelyj <belyjmaximteam3@gmail.com>
2017-10-04 00:11:43 +0300

    create class FlyNoWay

--- DeveloperPatterns/src/main/java/net/testingbehavior/BehaviorImpl/FlyNoWay.java
+++ DeveloperPatterns/src/main/java/net/testingbehavior/BehaviorImpl/FlyNoWay.java
@@ -2,7 +2,7 @@ package net.testingbehavior.BehaviorImpl;
 
 import net.testingbehavior.Behavior.FlyBehavior;
 
-public class FlyNoWay implements FlyBehavior{
+public class FlyNoWay implements FlyBehavior {
     public void fly() {
         System.out.println("I can't fly!");
     }
