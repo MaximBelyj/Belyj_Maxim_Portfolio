b0668c29cb8bf3b9f52f43512e0d050ff225cdf8
MaximBelyj <belyjmaximteam3@gmail.com>
2017-10-03 23:45:40 +0300

    create class Duck

---
+++ DeveloperPatterns/src/main/java/net/testingbehavior/Duck.java
@@ -0,0 +1,10 @@
+package net.testingbehavior;
+
+import net.testingbehavior.Behavior.FlyBehavior;
+import net.testingbehavior.Behavior.QuackBehavior;
+
+public abstract class Duck {
+
+    FlyBehavior flyBehavior;
+    QuackBehavior quackBehavior;
+}
