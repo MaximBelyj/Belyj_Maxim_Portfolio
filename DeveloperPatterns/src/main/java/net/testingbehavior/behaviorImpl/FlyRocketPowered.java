7c76585c010f0f556532d893fd68c5a21c2783c1
MaximBelyj <belyjmaximteam3@gmail.com>
2017-10-04 00:34:20 +0300

    create class FlyRocketPowered

---
+++ DeveloperPatterns/src/main/java/net/testingbehavior/behaviorImpl/FlyRocketPowered.java
@@ -0,0 +1,9 @@
+package net.testingbehavior.behaviorImpl;
+
+import net.testingbehavior.behavior.FlyBehavior;
+
+public class FlyRocketPowered implements FlyBehavior {
+    public void fly() {
+        System.out.println("I'm flying with a rocket!");
+    }
+}
