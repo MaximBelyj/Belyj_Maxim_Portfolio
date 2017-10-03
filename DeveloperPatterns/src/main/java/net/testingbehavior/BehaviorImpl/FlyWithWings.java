3a91717b0eae0efd1fcf901b448000628189726b
MaximBelyj <belyjmaximteam3@gmail.com>
2017-10-04 00:11:30 +0300

    create class FlyWithWings

--- DeveloperPatterns/src/main/java/net/testingbehavior/BehaviorImpl/FlyWithWings.java
+++ DeveloperPatterns/src/main/java/net/testingbehavior/BehaviorImpl/FlyWithWings.java
@@ -2,7 +2,7 @@ package net.testingbehavior.BehaviorImpl;
 
 import net.testingbehavior.Behavior.FlyBehavior;
 
-public class FlyWithWings implements FlyBehavior{
+public class FlyWithWings implements FlyBehavior {
     public void fly() {
         System.out.println("I'm flying!");
     }
